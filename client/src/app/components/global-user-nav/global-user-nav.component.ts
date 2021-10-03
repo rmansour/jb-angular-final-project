82d302929b5f02fe86b4ab2c5cfaed4558ecd0d8
Raed Mansour <raied272@gmail.com>
2021-10-04 01:45:22 +0300

    Eighth commit

--- client/src/app/components/global-user-nav/global-user-nav.component.ts
+++ client/src/app/components/global-user-nav/global-user-nav.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {UserAdminServiceService} from "../../_services/user-admin-service.service";
+import {Router} from '@angular/router';
+import Swal from 'sweetalert2';
+import {ShoppingCartItemsService} from '../../_services/shopping-cart-items.service';
+import {UserAdminServiceService} from '../../_services/user-admin-service.service';
 
 @Component({
   selector: 'app-global-user-nav',
@@ -8,10 +11,24 @@ import {UserAdminServiceService} from "../../_services/user-admin-service.servic
 })
 export class GlobalUserNavComponent implements OnInit {
 
-  constructor(public userAdminServiceService: UserAdminServiceService) {
+  constructor(public userAdminServiceService: UserAdminServiceService, public shoppingCartItemsService: ShoppingCartItemsService, private router: Router) {
   }
 
   ngOnInit(): void {
     this.userAdminServiceService.checkUser();
+  }
+
+  async showCartOnHomepage() {
+    if (this.shoppingCartItemsService.allShoppingCartItems.length !== 0)
+      await this.router.navigate(['/order-page']);
+    else
+      Swal.fire({
+        title: 'Oops...',
+        text: `It looks like your shopping cart is empty! Please add at least one item to your shopping cart!`,
+        icon: 'warning',
+        timer: 2500,
+        showCloseButton: true,
+        showConfirmButton: true
+      })
   }
 }
